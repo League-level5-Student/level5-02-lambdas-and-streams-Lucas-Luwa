d4792263c2e16bfa15eec8bd1b6b99a459a96d5e
Luki <>
2019-04-03 17:17:43 -0700

    Finished
    4/3/19

--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -10,29 +10,20 @@ public class StreamSearcher {
 	// 1. This method takes an array of Strings and a String. The method returns the
 	// number of times the String parameter appears in the String array. This method
 	// will use Streams to help us out.
+	// 2. Convert the String array to a stream
 	public long containsCount(String[] strArr, String strToCount) {
-		int l = 0;
-		// 2. Convert the String array to a stream
-		Stream<String> sm = Arrays.stream(strArr);
+		return Arrays.stream(strArr).filter((i) -> {
+			if (i.equals(strToCount)) {
+				return true;
+			} else {
+				return false;
+			}
+		}).count();
 		// 3. Use the filter method with a lambda to extract all the String
 		// elements that match the String parameter.
-		// sm.filter((i) -> {
-		// if (i.equals(strToCount)) {
-		// //l+=1;
-		// return true;
-		// } else {
-		// return false;
-		// }
-		// });
-		sm.forEach((i) -> {
-			if (i.equals(strToCount)) {
-			l+=1;
-			} else {
-			}
-
-		});
+		
 		// 4. Return the count of the filtered stream.
-		System.out.println(sm.count());
+		
 
 	}
 }
